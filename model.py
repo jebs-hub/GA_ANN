3631078643dc4ee9ecae0be2d78e47eb36e40437
jess <j173931@dac.unicamp.br>
2023-01-29 11:58:59 -0300

    created basic function for classes of organisms and ecosystem

--- model.py
+++ model.py
@@ -3,10 +3,14 @@ import random
 
 class Organisms:
     
-    def __init__(self):
+    def __init__(self, idx, neural_net=None):
         self.number_of_feeding = 0
-        self.time_aliving = 0
-        self.nn = NeuralNetwork(number_of_neurons_per_layer=[3,4,5,4])
+        self.idx = idx
+        self.time_alive = 0
+        if(neural_net == None):
+            self.nn = NeuralNetwork(number_of_neurons_per_layer=[3,4,5,4])
+        else: 
+            self.nn = neural_net
 
     def increment_number_of_feeding(self):
         self.number_of_feeding+=1
@@ -16,46 +20,57 @@ class Organisms:
         self.number_of_feeding = 0
     
 
-    def set_time_aliving(self, time): 
+    def set_time_alive(self, time): 
         self.time_alive = time
-    
-
-    #def copy(self):
-    #    new_neural_net = []
-    #    for layer in self.neural_net:
-    #        new_layer = []
-    #        for neuron in layer:
-    #            new_neuron = neuron.copy()
-    #            new_layer.append(new_neuron)
-    #        new_neural_net.append(new_layer)
-    #    return NeuralNetwork(self.number_of_layers, self.number_of_neurons_per_layer,new_neural_net)
-
 
     def copy_with_mutation(self):
-        new_neural_net = []        
-        for layer in self.neural_net[:-1]:  
-            new_layer = []
-            for neuron in layer:
-                sort = random.randint(0,2) #zero or 1
-                if(sort == 0):
-                    new_neuron = neuron.copy() #don't mutate
-                else:
-                    new_neuron = neuron.copy_with_mutation()
-                new_layer.append(new_neuron)
-            new_neural_net.append(new_layer)
-            new_layer = []
-            for neuron in self.neural_net[-1]:
-                sort = random.randint(0,2) #zero or 1
-                if(sort == 0):
-                    new_neuron = neuron.copy() #don't mutate
-                else:
-                    new_neuron = neuron.copy()
-                    new_neuron.set_bias(random.uniform(-5,5))
-                new_layer.append(new_neuron)
-                new_neural_net.append(new_layer)
+        new_nn = self.nn.copy_with_mutation()
+        return Organisms(new_nn)
+    
+    def isSelected(self):
+        if(self.time_alive>=5 and self.number_of_feeding>=2):
+            return True
+        else:
+            return False
+    
+    def get_command(self):
+        output = self.nn.get_output()
+        if(output[0]==1):
+            return "up"
+        if(output[1]==1):
+            return "down"
+        if(output[2]==1):
+            return "right"
+        if(output[3]==1):
+            return "left"
 
-        return NeuralNetwork(self.number_of_layers, self.number_of_neurons_per_layer,new_neural_net)
+
 
 class Ecosystem:
-    pass
+    def __init__(self, number_of_organisms):
+        self.number_of_organisms = number_of_organisms
+        self.organisms = []
+        for i in range(number_of_organisms):
+            self.organisms.append(Organisms(i))
+        self.abs_vel = 2
+    
+    def select(self):
+        new_organisms = []
+        number_of_organisms = 0
+        for i in range(self.number_of_organisms):
+            if(self.organisms[i].isSelected()):
+                new_organism = self.organisms[i].copy_with_mutation()
+                new_organisms.append(new_organism)
+                new_organisms.append(self.organisms[i])
+                number_of_organisms+=2
+        self.number_of_organisms = number_of_organisms
+        self.organisms = new_organisms
+    
+    def feed(self,idx):
+        self.organisms[idx].increment_feeding()
 
+    def set_time_alive(self,idx, time):
+        self.organisms[idx].set_time_alive(time)
+    
+    def get_command(self, idx):
+        return self.organisms[idx].get_command()
