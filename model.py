c5478ee313f827805ba5cc8bde8e1a4ca2d74c6b
jess <j173931@dac.unicamp.br>
2023-01-31 10:22:12 -0300

    refactored the model, deleted useless information

--- model.py
+++ model.py
@@ -1,19 +1,27 @@
 from nn_library.nn import NeuralNetwork
-import random
+
+#Parameters
+nn_eskeleton = [8,6,4] #default neural net for our model
 
 class OrganismBrain:
     
+
     def __init__(self,neural_net=None):
         self.number_of_feeding = 0
         self.time_alive = 0
         if(neural_net == None):
-            self.nn = NeuralNetwork(number_of_neurons_per_layer=[8,6,4])
+            self.nn = NeuralNetwork(number_of_neurons_per_layer=nn_eskeleton)
         else: 
             self.nn = neural_net
+
 
     def increment_number_of_feeding(self):
         self.number_of_feeding+=1
     
+
+    def get_number_of_feeding(self):
+        return self.number_of_feedings
+
 
     def reset_number_of_feeding(self):
         self.number_of_feeding = 0
@@ -21,21 +29,22 @@ class OrganismBrain:
 
     def set_time_alive(self, time): 
         self.time_alive = time
+    
+
+    def get_time_alive(self):
+        return self.time_alive
+
 
     def copy_with_mutation(self):
         new_nn = self.nn.copy_with_mutation()
         return OrganismBrain(neural_net=new_nn)
-    
-    def isSelected(self):
-        if(self.time_alive>=5 and self.number_of_feeding>=2):
-            return True
-        else:
-            return False
-    
-    def sense_environment(self, coords):
+
+
+    def get_environment_info(self, coords):
         self.nn.input_data(coords)
     
-    def get_command(self):
+
+    def response(self):
         self.nn.run_net()
         output = self.nn.get_output()
         max = output[0]
@@ -52,34 +61,3 @@ class OrganismBrain:
             return "right"
         if(idx==3):
             return "left"
-
-
-
-class Ecosystem:
-    def __init__(self, number_of_organisms):
-        self.number_of_organisms = number_of_organisms
-        self.organisms = []
-        for i in range(number_of_organisms):
-            self.organisms.append(Organisms(i))
-        self.abs_vel = 2
-    
-    def select(self):
-        new_organisms = []
-        number_of_organisms = 0
-        for i in range(self.number_of_organisms):
-            if(self.organisms[i].isSelected()):
-                new_organism = self.organisms[i].copy_with_mutation()
-                new_organisms.append(new_organism)
-                new_organisms.append(self.organisms[i])
-                number_of_organisms+=2
-        self.number_of_organisms = number_of_organisms
-        self.organisms = new_organisms
-    
-    def feed(self,idx):
-        self.organisms[idx].increment_feeding()
-
-    def set_time_alive(self,idx, time):
-        self.organisms[idx].set_time_alive(time)
-    
-    def get_command(self, idx):
-        return self.organisms[idx].get_command()
